f3f250a28cee171cdbdfe3e47ad7f434d7547e3b
Jake Franke <113054625+jccool16@users.noreply.github.com>
2024-02-29 14:13:01 -0700

    React Router Implemntation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import logo from './maxwell-cat.gif';
-import './App.css';
-import {SocialIcon} from 'react-social-icons';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from '../src/pages/home/Home';
 
 function App() {
   return (
-    <div className="App" >
-      <header className="App-header" style={{marginLeft: 30}}>
-
-        <img src={logo} className="Spinning-cat" alt="maxwell" style={{alignSelf: "center"}}/>
-
-        <p style={{fontSize: 40, margin: 0}}>
-          Jackson Franke
-        </p>
-
-        <p style={{fontSize: 20, fontWeight: 100, whiteSpace: "pre-wrap"}}>
-          BS in Computer Systems Engineering<br/>
-          Arizona State University
-        </p>
-
-        <p style={{fontSize: 20}}>
-        </p>
-
-        <p>
-          <SocialIcon target="_blank" url="https://www.linkedin.com/in/jake-franke-9195b01ba"/>
-          &nbsp;
-          <SocialIcon target="_blank" url="https://github.com/jccool16"/>
-          &nbsp;
-          <SocialIcon target="_blank" href="https://www.discordapp.com/users/510270107604484128" url="https://www.discord.com"/>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Home/>
+    </BrowserRouter>
   );
 }
 
