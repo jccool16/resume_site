97c0b4f302d5f609ecece75859affef1c2be40e2
jccool16 <jccool16@hotmail.com>
2025-02-20 13:37:06 -0700

    Create Music Page

---
+++ src/pages/music/Music.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './music.css'
+
+const Music = props => {
+    return (        
+    <div className="Music" >
+        <h className="Music-name">
+            <h1 className="Music-name header">
+                This is where the music goes
+            </h1>
+        </h>
+    </div>
+    )
+}
+
+export default Music
+
+			
+
